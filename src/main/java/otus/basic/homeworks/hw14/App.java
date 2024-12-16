89ebe9a29613827f5ed53ae039b6f38e1e483cd7
Yanin Vyacheslav <slava.vy.2006@gmail.com>
2024-12-16 19:22:19 +0300

    HW14 add join for threads

--- src/main/java/otus/basic/homeworks/hw14/App.java
+++ src/main/java/otus/basic/homeworks/hw14/App.java
@@ -5,29 +5,29 @@ public class App {
     public static void main(String[] args) {
         long start, end;
         double[] arr = new double[100_000_000];
+        double[] arr1 = arr.clone();
         start = System.currentTimeMillis();
         makeArray(arr);
         end = System.currentTimeMillis();
         System.out.println(((end - start)) + " millisecond");
         System.out.println("---------------------");
         start = System.currentTimeMillis();
-        makeArrayWithTreads(arr);
+        makeArrayWithThreads(arr1);
         end = System.currentTimeMillis();
         System.out.println(((end - start)) + " millisecond");
     }
 
     public static void makeArray(double[] arr) {
-        for (int i = 0; i < arr.length / 4; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
         }
     }
 
-    public static void makeArrayWithTreads(double[] arr) {
+    public static void makeArrayWithThreads(double[] arr) {
         int quater = arr.length / 4;
         Thread t1 = new Thread(() -> {
             for (int i = 0; i < quater; i++) {
                 arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-
             }
         });
         Thread t2 = new Thread(() -> {
@@ -49,6 +49,13 @@ public class App {
         t2.start();
         t3.start();
         t4.start();
-
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
