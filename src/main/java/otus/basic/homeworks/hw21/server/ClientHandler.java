c56bb97f907389c51d12260cccf803db01059acf
Yanin Vyacheslav <138291033+sygkanrt30@users.noreply.github.com>
2025-01-17 16:40:34 +0300

    Delete src/main/java/otus/basic/homeworks/hw21/server directory

--- src/main/java/otus/basic/homeworks/hw21/server/ClientHandler.java
+++
@@ -1,9 +0,0 @@
-package otus.basic.homeworks.hw21.server;
-
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.net.Socket;
-
-public record ClientHandler(Socket clientSocket, DataInputStream inputStream, DataOutputStream outputStream){}
-
-
